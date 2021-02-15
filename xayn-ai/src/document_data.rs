e60f8b3e5dcfb0f5012c59c191af570c31355708
Andrea Corradi <andrea.corradi@xayn.com>
2021-02-15 13:40:42 +0100

    Add DocumentData structure

---
+++ xayn-ai/src/document_data.rs
@@ -0,0 +1,219 @@
+#![allow(dead_code)]
+
+#[repr(transparent)]
+#[cfg_attr(test, derive(Debug, PartialEq, Clone))]
+pub struct DocumentId(pub String);
+
+#[cfg_attr(test, derive(Debug, PartialEq, Clone))]
+pub struct DocumentIdComponent {
+    pub id: DocumentId,
+}
+
+#[cfg_attr(test, derive(Debug, PartialEq, Clone))]
+pub struct DocumentContentComponent {
+    pub snippet: String,
+}
+
+#[cfg_attr(test, derive(Debug, PartialEq, Clone))]
+pub struct EmbeddingComponent {
+    pub embedding: Vec<f32>,
+}
+
+#[cfg_attr(test, derive(Debug, PartialEq, Clone))]
+pub struct LtrComponent {
+    pub context_value: f32,
+}
+
+#[repr(transparent)]
+#[cfg_attr(test, derive(Debug, PartialEq, Clone))]
+pub struct CenterOfInterestId(pub usize);
+
+#[cfg_attr(test, derive(Debug, PartialEq, Clone))]
+pub struct CenterOfInterestComponent {
+    pub id: CenterOfInterestId,
+    /// Distance from the positive center of interest
+    pub pos_distance: f32,
+    /// Distance from the negative center of interest
+    pub neg_distance: f32,
+}
+
+#[cfg_attr(test, derive(Debug, PartialEq, Clone))]
+pub struct ContextComponent {
+    pub context_value: f32,
+}
+
+#[cfg_attr(test, derive(Debug, PartialEq, Clone))]
+pub struct MabComponent {
+    pub rank: usize,
+}
+
+// Document usage order:
+// DocumentDataWithDocument -> DocumentDataWithEmbedding -> DocumentDataWithCenterOfInterest ->
+// DocumentDataWithLtr -> DocumentDataWithContext -> DocumentDataWithMab
+
+pub struct DocumentDataWithDocument {
+    pub document_id: DocumentIdComponent,
+    pub document_content: DocumentContentComponent,
+}
+
+pub struct DocumentDataWithEmbedding {
+    document_id: DocumentIdComponent,
+    embedding: EmbeddingComponent,
+}
+
+impl DocumentDataWithEmbedding {
+    pub fn from_document(
+        document: DocumentDataWithDocument,
+        embedding: EmbeddingComponent,
+    ) -> Self {
+        Self {
+            document_id: document.document_id,
+            embedding,
+        }
+    }
+}
+
+pub struct DocumentDataWithCenterOfInterest {
+    pub document_id: DocumentIdComponent,
+    pub embedding: EmbeddingComponent,
+    pub center_of_interest: CenterOfInterestComponent,
+}
+
+impl DocumentDataWithCenterOfInterest {
+    pub fn from_document(
+        document: DocumentDataWithEmbedding,
+        center_of_interest: CenterOfInterestComponent,
+    ) -> Self {
+        Self {
+            document_id: document.document_id,
+            embedding: document.embedding,
+            center_of_interest,
+        }
+    }
+}
+
+pub struct DocumentDataWithLtr {
+    pub document_id: DocumentIdComponent,
+    pub embedding: EmbeddingComponent,
+    pub center_of_interest: CenterOfInterestComponent,
+    pub ltr: LtrComponent,
+}
+
+impl DocumentDataWithLtr {
+    pub fn from_document(document: DocumentDataWithCenterOfInterest, ltr: LtrComponent) -> Self {
+        Self {
+            document_id: document.document_id,
+            embedding: document.embedding,
+            center_of_interest: document.center_of_interest,
+            ltr,
+        }
+    }
+}
+
+pub struct DocumentDataWithContext {
+    pub document_id: DocumentIdComponent,
+    pub embedding: EmbeddingComponent,
+    pub center_of_interest: CenterOfInterestComponent,
+    pub ltr: LtrComponent,
+    pub context: ContextComponent,
+}
+
+impl DocumentDataWithContext {
+    pub fn from_document(document: DocumentDataWithLtr, context: ContextComponent) -> Self {
+        Self {
+            document_id: document.document_id,
+            embedding: document.embedding,
+            center_of_interest: document.center_of_interest,
+            ltr: document.ltr,
+            context,
+        }
+    }
+}
+
+pub struct DocumentDataWithMab {
+    pub document_id: DocumentIdComponent,
+    pub embedding: EmbeddingComponent,
+    pub center_of_interest: CenterOfInterestComponent,
+    pub ltr: LtrComponent,
+    pub context: ContextComponent,
+    pub mab: MabComponent,
+}
+
+impl DocumentDataWithMab {
+    pub fn from_document(document: DocumentDataWithContext, mab: MabComponent) -> Self {
+        Self {
+            document_id: document.document_id,
+            embedding: document.embedding,
+            center_of_interest: document.center_of_interest,
+            ltr: document.ltr,
+            context: document.context,
+            mab,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn transition_and_get() {
+        let document_id = DocumentIdComponent {
+            id: DocumentId("id".to_string()),
+        };
+        let document_content = DocumentContentComponent {
+            snippet: "snippet".to_string(),
+        };
+        let document_data = DocumentDataWithDocument {
+            document_id: document_id.clone(),
+            document_content: document_content.clone(),
+        };
+        assert_eq!(document_data.document_id, document_id);
+        assert_eq!(document_data.document_content, document_content);
+
+        let embedding = EmbeddingComponent {
+            embedding: vec![1., 2., 3., 4.],
+        };
+        let document_data =
+            DocumentDataWithEmbedding::from_document(document_data, embedding.clone());
+        assert_eq!(document_data.document_id, document_id);
+        assert_eq!(document_data.embedding, embedding);
+
+        let coi = CenterOfInterestComponent {
+            id: CenterOfInterestId(9),
+            pos_distance: 0.7,
+            neg_distance: 0.2,
+        };
+        let document_data =
+            DocumentDataWithCenterOfInterest::from_document(document_data, coi.clone());
+        assert_eq!(document_data.document_id, document_id);
+        assert_eq!(document_data.embedding, embedding);
+        assert_eq!(document_data.center_of_interest, coi);
+
+        let ltr = LtrComponent { context_value: 0.3 };
+        let document_data = DocumentDataWithLtr::from_document(document_data, ltr.clone());
+        assert_eq!(document_data.document_id, document_id);
+        assert_eq!(document_data.embedding, embedding);
+        assert_eq!(document_data.center_of_interest, coi);
+        assert_eq!(document_data.ltr, ltr);
+
+        let context = ContextComponent {
+            context_value: 1.23,
+        };
+        let document_data = DocumentDataWithContext::from_document(document_data, context.clone());
+        assert_eq!(document_data.document_id, document_id);
+        assert_eq!(document_data.embedding, embedding);
+        assert_eq!(document_data.center_of_interest, coi);
+        assert_eq!(document_data.ltr, ltr);
+        assert_eq!(document_data.context, context);
+
+        let mab = MabComponent { rank: 3 };
+        let document_data = DocumentDataWithMab::from_document(document_data, mab.clone());
+        assert_eq!(document_data.document_id, document_id);
+        assert_eq!(document_data.embedding, embedding);
+        assert_eq!(document_data.center_of_interest, coi);
+        assert_eq!(document_data.ltr, ltr);
+        assert_eq!(document_data.context, context);
+        assert_eq!(document_data.mab, mab);
+    }
+}
