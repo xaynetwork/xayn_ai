57b406ff80d0fc10490116e5f23072618ecb89aa
Andrea Corradi <andrea.corradi@xayn.com>
2021-02-18 00:04:34 +0100

    Add script to download rubert model

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Xayn-AI
 
+## Models
+To download the models use the `download_data.sh` script.
+
---
+++ download_data.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# path to this file
+SELF_PATH=$(readlink -f "$0")
+# path to the directory where this file is
+SELF_DIR_PATH=$(dirname "$SELF_PATH")
+
+# in this way we can call the script from different directory
+# but the data should go in the correct destination
+DATA_DIR="$SELF_DIR_PATH/data/"
+
+download_rubert()
+{
+
+  VERSION=$1
+  ARCHIVE="rubert_${VERSION}.tgz"
+  URL="http://s3-de-central.profitbricks.com/xayn-yellow-bert/rubert/$ARCHIVE"
+
+  curl $URL -o $DATA_DIR/$ARCHIVE
+
+  cd $DATA_DIR
+  tar -zxf $ARCHIVE
+}
+
+download_rubert v0000
+
