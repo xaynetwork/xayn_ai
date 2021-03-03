a315b33353c441a7cb44ac6cea28d4951e7c8d9d
Kwok Cheung <kwok.doc@gmail.com>
2021-03-03 19:09:35 +0100

    unit test
    allow(clippy::float_cmp)

--- xayn-ai/src/context.rs
+++ xayn-ai/src/context.rs
@@ -56,3 +56,81 @@ impl ContextCalc {
         (frac_pos + frac_neg + ltr_score) / wt_div
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::data::{
+        document::DocumentId,
+        document_data::{CoiComponent, DocumentIdComponent, EmbeddingComponent, LtrComponent},
+        CoiId,
+    };
+
+    struct LtrDocBuilder {
+        docs: Vec<DocumentDataWithLtr>,
+    }
+
+    impl LtrDocBuilder {
+        fn new() -> Self {
+            Self { docs: vec![] }
+        }
+
+        fn add_doc(&mut self, ltr_score: f32, pos_distance: f32, neg_distance: f32) {
+            let id = DocumentId("id".to_string());
+            let embedding = vec![].into();
+
+            self.docs.push(DocumentDataWithLtr {
+                document_id: DocumentIdComponent { id },
+                embedding: EmbeddingComponent { embedding },
+                coi: CoiComponent {
+                    id: CoiId(0),
+                    pos_distance,
+                    neg_distance,
+                },
+                ltr: LtrComponent { ltr_score },
+            });
+        }
+    }
+
+    #[allow(clippy::float_cmp)]
+    #[test]
+    fn test_calculate() {
+        let calc = ContextCalc {
+            pos_avg: 4.,
+            neg_max: 8.,
+        };
+        let cxt = calc.calculate(0., 0., calc.neg_max);
+        assert_eq!(cxt, 2. / 3.); // 1/3 + 1/3
+        let cxt = calc.calculate(1., 0., calc.neg_max);
+        assert_eq!(cxt, 1.); // 1/3 + 1/3 + 1/3
+        let cxt = calc.calculate(0., calc.pos_avg, calc.neg_max);
+        assert_eq!(cxt, 0.5); // 1/6 + 1/3
+        let cxt = calc.calculate(0., 8., 7.);
+        assert_eq!(cxt, 5. / 18.) // 1/9 + 1/6
+    }
+
+    #[allow(clippy::float_cmp)]
+    #[test]
+    fn test_compute_from_docs() {
+        let mut ltr_docs = LtrDocBuilder::new();
+        ltr_docs.add_doc(0.9, 1., 10.);
+        ltr_docs.add_doc(0.5, 6., 4.);
+        ltr_docs.add_doc(0.3, 8., 2.);
+        // pos_avg = 5, neg_max = 10
+
+        let cxt_docs = Context.compute_context(ltr_docs.docs);
+        assert!(cxt_docs.is_ok());
+        let cxt_docs = cxt_docs.unwrap();
+        assert_eq!(cxt_docs.len(), 3);
+
+        assert_eq!(cxt_docs[0].context.context_value, (5. / 6. + 1. + 0.9) / 3.);
+        assert_eq!(
+            cxt_docs[1].context.context_value,
+            (5. / 11. + 1. / 7. + 0.5) / 3.
+        );
+        assert_eq!(
+            cxt_docs[2].context.context_value,
+            (5. / 13. + 1. / 9. + 0.3) / 3.
+        );
+    }
+}
