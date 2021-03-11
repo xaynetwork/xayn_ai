3e86c37d98e93b40b3a441bdeaa0dbe0f4224f75
Andrea Corradi <andrea.corradi@xayn.com>
2021-03-11 11:54:12 +0100

    Update xayn-ai/src/mab.rs
    Co-authored-by: kwok <kwok.cheung@xain.io>

--- xayn-ai/src/mab.rs
+++ xayn-ai/src/mab.rs
@@ -156,7 +156,7 @@ where
     })
 }
 
-/// For each coi with documents we take a sample from the beta distribution and we pick
+/// For each coi we take a sample from the beta distribution and we pick
 /// the coi with the biggest sample. Then we take the document with the biggest `context_value` among
 /// the documents within that coi.
 fn pull_arms<T>(
