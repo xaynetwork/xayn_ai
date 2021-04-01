6856fc6a066e7c07083f97a4b5d24994415f8ab1
Jan Petsche <58227040+janpetschexain@users.noreply.github.com>
2021-04-01 12:10:52 +0200

    update build script

--- xayn-ai-ffi-c/build.rs
+++ xayn-ai-ffi-c/build.rs
@@ -1,6 +1,23 @@
-use std::{env, fs::read_dir, path::PathBuf};
+use std::{
+    env,
+    fs::read_dir,
+    path::{Path, PathBuf},
+};
 
-use cbindgen::{Builder, Config};
+use cbindgen::{generate_with_config, Config};
+
+// cargo doesn't check directories recursively so we have to do it by hand, also emitting a
+// rerun-if line cancels the default rerun for changes in the crate directory
+fn cargo_rerun_if_changed(entry: impl AsRef<Path>) {
+    let entry = entry.as_ref();
+    if entry.is_dir() {
+        for entry in read_dir(entry).expect("Failed to read dir.") {
+            cargo_rerun_if_changed(entry.expect("Failed to read entry.").path());
+        }
+    } else {
+        println!("cargo:rerun-if-changed={}", entry.display());
+    }
+}
 
 fn main() {
     let crate_dir = PathBuf::from(
@@ -9,25 +26,12 @@ fn main() {
     let bind_config = crate_dir.join("cbindgen.toml");
     let bind_file = crate_dir.join("ffi.h");
 
-    // cargo doesn't check directories recursively so we have to do it by hand, also emitting a
-    // rerun-if line cancels the default rerun for changes in the crate directory
-    let src_dir = crate_dir.join("src");
-    for file in read_dir(src_dir.as_path()).expect("Failed to read src dir.") {
-        println!(
-            "cargo:rerun-if-changed={}",
-            src_dir
-                .join(file.expect("Failed to read src file.").file_name())
-                .display()
-        );
-    }
-    println!("cargo:rerun-if-changed=Cargo.toml");
-    println!("cargo:rerun-if-changed={}", bind_config.display());
+    cargo_rerun_if_changed(crate_dir.join("src"));
+    cargo_rerun_if_changed(crate_dir.join("Cargo.toml"));
+    cargo_rerun_if_changed(bind_config.as_path());
 
-    // generate bindings
     let config = Config::from_file(bind_config).expect("Failed to read config.");
-    let builder = Builder::new().with_crate(crate_dir).with_config(config);
-    builder
-        .generate()
+    generate_with_config(crate_dir, config)
         .expect("Failed to generate bindings.")
         .write_to_file(bind_file);
 }
