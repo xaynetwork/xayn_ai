b45597112311ffa0a9059217ffc51ce2f79cb413
Kwok Cheung <kwok.doc@gmail.com>
2021-04-13 11:18:49 +0200

    query features
    fixed iterator bugs; added mrr

--- xayn-ai/src/features/dataiku.rs
+++ xayn-ai/src/features/dataiku.rs
@@ -18,18 +18,85 @@ struct UserFeatures {
 
 struct QueryFeatures {
     click_entropy: f32,
-    num_terms: u32,
-    /// Average rank per session.
-    rank_per_session: u32,
+    num_terms: usize,
+    /// Average `n` where query is the `n`th of a session.
+    rank_per_session: f32,
     /// Average number of occurrences per session.
-    occurs_per_session: u32,
+    occurs_per_session: f32,
     /// Total number of occurrences.
-    num_occurs: u32,
+    num_occurs: usize,
     click_mrr: f32,
     /// Average number of clicks.
-    avg_clicks: u32,
+    avg_clicks: f32,
     /// Average number of skips.
-    avg_skips: u32,
+    avg_skips: f32,
+}
+
+fn query_features(history: &[SearchResult], query: Query) -> QueryFeatures {
+    let history_q = history
+        .iter()
+        .filter(|r| r.query_id == query.id)
+        .collect_vec();
+
+    let click_entropy = click_entropy(&history_q);
+
+    let occurs = history_q
+        .iter()
+        .map(|r| (r.session_id, r.query_counter))
+        .collect::<HashSet<_>>();
+
+    let num_occurs = occurs.len();
+
+    let rank_sum = occurs
+        .into_iter()
+        .map(|(_, query_count)| query_count)
+        .sum::<u8>() as f32;
+    let rank_per_session = rank_sum / num_occurs as f32;
+
+    let num_sessions = history_q.iter().unique_by(|r| r.session_id).count() as f32;
+    let occurs_per_session = num_occurs as f32 / num_sessions;
+
+    let clicked = history_q
+        .iter()
+        .filter(|r| r.relevance > ClickScore::Low)
+        .collect_vec();
+    let click_mrr = mrr(&clicked);
+
+    let avg_clicks = clicked.len() as f32 / num_occurs as f32;
+
+    let avg_skips = history_q
+        .into_iter()
+        .filter(|r| r.relevance == ClickScore::Low)
+        .count() as f32
+        / num_occurs as f32;
+
+    QueryFeatures {
+        click_entropy,
+        num_terms: query.words.len(),
+        rank_per_session,
+        occurs_per_session,
+        num_occurs,
+        click_mrr,
+        avg_clicks,
+        avg_skips,
+    }
+}
+
+/// Mean reciprocal rank of the given results.
+fn mrr(results: &[impl AsRef<SearchResult>]) -> f32 {
+    let denom = 1. + results.len() as f32;
+    let numer = 0.283
+        + results
+            .into_iter()
+            .map(|r| f32::from(r.as_ref().position).recip())
+            .sum::<f32>();
+
+    numer / denom
+}
+
+struct Query {
+    id: i32,
+    words: Vec<i32>,
 }
 
 #[derive(Clone)]
@@ -46,14 +113,20 @@ struct SearchResult {
     query_counter: u8,
 }
 
-/// Yandex notion of dwell-time.
+impl AsRef<SearchResult> for SearchResult {
+    fn as_ref(&self) -> &SearchResult {
+        self
+    }
+}
+
+/// Yandex notion of dwell-time: time elapsed between a click and the next action.
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 enum ClickScore {
     /// Less than 50 units of time.
     Low = 0,
     /// From 50 to 300 units of time.
     Medium,
-    /// More than 300 units of time.
+    /// More than 300 units of time, or last click of the session.
     High,
 }
 
@@ -78,6 +151,12 @@ enum Rank {
     Eighth,
     Nineth,
     Tenth,
+}
+
+impl From<Rank> for f32 {
+    fn from(rank: Rank) -> Self {
+        rank as u8 as f32
+    }
 }
 
 /// Counts the variety of query terms over a given test session.
@@ -114,10 +193,10 @@ fn prior(outcome: UrlFeedback) -> u8 {
     }
 }
 
-fn click_entropy(results: &[SearchResult]) -> f32 {
+fn click_entropy(results: &[impl AsRef<SearchResult>]) -> f32 {
     let rank_freqs = results
         .iter()
-        .filter_map(|r| (r.relevance > ClickScore::Low).then(|| r.position))
+        .filter_map(|r| (r.as_ref().relevance > ClickScore::Low).then(|| r.as_ref().position))
         .counts();
 
     let freqs_sum = rank_freqs.values().sum::<usize>() as f32;
@@ -164,9 +243,6 @@ fn click_counts(results: &[SearchResult]) -> ClickCounts {
 }
 
 fn user_features(history: &[SearchResult]) -> UserFeatures {
-    let click_entropy = click_entropy(history);
-    let click_counts = click_counts(history);
-
     let all_queries = history
         .iter()
         .map(|r| (r.session_id, r.query_id, &r.query_words, r.query_counter))
@@ -198,8 +274,8 @@ fn user_features(history: &[SearchResult]) -> UserFeatures {
         / num_sessions as f32;
 
     UserFeatures {
-        click_entropy,
-        click_counts,
+        click_entropy: click_entropy(history),
+        click_counts: click_counts(history),
         num_queries,
         words_per_query,
         words_per_session,
