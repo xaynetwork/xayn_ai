3495121fc9d045a08646e488c61927cab41ab056
Robert Debug <robertt.debug@gmail.com>
2021-04-22 19:23:19 +0200

    move fn into impl block

--- xayn-ai-ffi-wasm/src/ai.rs
+++ xayn-ai-ffi-wasm/src/ai.rs
@@ -4,68 +4,76 @@ use js_sys::Uint8Array;
 use wasm_bindgen::prelude::{wasm_bindgen, JsValue};
 use xayn_ai::{Builder, Document, DocumentHistory, Reranker};
 
-use super::utils::ToJsResult;
+use super::utils::{ExternError, IntoJsResult};
 
 #[wasm_bindgen]
 pub struct WXaynAi(Reranker);
 
 #[wasm_bindgen]
-pub fn xaynai_new(
-    vocab: &[u8],
-    model: &[u8],
-    serialized: Option<Box<[u8]>>,
-) -> Result<WXaynAi, JsValue> {
-    Builder::default()
-        .with_serialized_database(&serialized.unwrap_or_default())
-        .to_js_result()?
-        .with_bert_from_reader(vocab, model)
-        .build()
-        .map(WXaynAi)
-        .to_js_result()
-}
+impl WXaynAi {
+    #[wasm_bindgen(constructor)]
+    pub fn new(
+        vocab: &[u8],
+        model: &[u8],
+        serialized: Option<Box<[u8]>>,
+    ) -> Result<WXaynAi, JsValue> {
+        Builder::default()
+            .with_serialized_database(&serialized.unwrap_or_default())
+            .map_err(|cause| ExternError {
+                code: 1,
+                msg: format!("Failed to deserialize the reranker database: {}", cause),
+            })
+            .into_js_result()?
+            .with_bert_from_reader(vocab, model)
+            .build()
+            .map(WXaynAi)
+            .map_err(|cause| ExternError {
+                code: 2,
+                msg: format!("Failed to initialize the ai: {}", cause),
+            })
+            .into_js_result()
+    }
 
-#[wasm_bindgen]
-pub fn xaynai_rerank(
-    xaynai: &mut WXaynAi,
-    // Vec<JsValue> behaves like Box<[JsValue]> here
-    // https://rustwasm.github.io/wasm-bindgen/api/wasm_bindgen/convert/trait.FromWasmAbi.html#impl-FromWasmAbi-for-Box%3C%5BJsValue%5D%3E
-    history: Vec<JsValue>,
-    documents: Vec<JsValue>,
-) -> Result<Vec<u32>, JsValue> {
-    // https://rustwasm.github.io/docs/wasm-bindgen/reference/arbitrary-data-with-serde.html
-    let history = history
-        .iter()
-        .map(JsValue::into_serde)
-        .collect::<Result<Vec<DocumentHistory>, _>>()
-        .to_js_result()?;
-    let documents = documents
-        .iter()
-        .map(|d| d.into_serde())
-        .collect::<Result<Vec<Document>, _>>()
-        .to_js_result()?;
-    let reranked = xaynai.0.rerank(&history, &documents);
+    pub fn rerank(
+        &mut self,
+        // Vec<JsValue> behaves like Box<[JsValue]> here
+        // https://rustwasm.github.io/wasm-bindgen/api/wasm_bindgen/convert/trait.FromWasmAbi.html#impl-FromWasmAbi-for-Box%3C%5BJsValue%5D%3E
+        history: Vec<JsValue>,
+        documents: Vec<JsValue>,
+    ) -> Result<Vec<u32>, JsValue> {
+        let history = history
+            .iter()
+            .map(JsValue::into_serde)
+            .collect::<Result<Vec<DocumentHistory>, _>>()
+            .into_js_result()?;
+        let documents = documents
+            .iter()
+            .map(JsValue::into_serde)
+            .collect::<Result<Vec<Document>, _>>()
+            .into_js_result()?;
+        let reranked = self.0.rerank(&history, &documents);
 
-    let ranks = reranked
-        .into_iter()
-        .map(|(id, rank)| (id, rank as u32))
-        .collect::<HashMap<_, _>>();
-    documents
-        .iter()
-        .map(|document| ranks.get(&document.id).copied())
-        .collect::<Option<Vec<_>>>()
-        .ok_or_else(|| {
-            JsValue::from_str("Failed to rerank the documents: The document ids are inconsistent")
-        })
-}
+        let ranks = reranked
+            .into_iter()
+            .map(|(id, rank)| (id, rank as u32))
+            .collect::<HashMap<_, _>>();
+        documents
+            .iter()
+            .map(|document| ranks.get(&document.id).copied())
+            .collect::<Option<Vec<_>>>()
+            .ok_or_else(|| {
+                JsValue::from_str(
+                    "Failed to rerank the documents: The document ids are inconsistent",
+                )
+            })
+    }
 
-#[wasm_bindgen]
-pub fn xaynai_serialize(xaynai: &WXaynAi) -> Result<Uint8Array, JsValue> {
-    // https://github.com/rustwasm/wasm-bindgen/issues/2402#issuecomment-754032667
-    xaynai
-        .0
-        .serialize()
-        .to_js_result()
-        .map(|bytes| bytes.as_slice().into())
+    pub fn serialize(&self) -> Result<Uint8Array, JsValue> {
+        self.0
+            .serialize()
+            .into_js_result()
+            .map(|bytes| bytes.as_slice().into())
+    }
 }
 
 #[cfg(target_arch = "wasm32")]
@@ -83,7 +91,7 @@ mod tests {
 
     #[wasm_bindgen_test]
     fn test_reranker() {
-        let mut xaynai = xaynai_new(VOCAB, MODEL, None).unwrap();
+        let mut xaynai = WXaynAi::new(VOCAB, MODEL, None).unwrap();
 
         let document = Document {
             id: "1".into(),
@@ -92,10 +100,10 @@ mod tests {
         };
         let js_document = JsValue::from_serde(&document).unwrap();
 
-        let ranks = xaynai_rerank(&mut xaynai, vec![], vec![js_document]).unwrap();
+        let ranks = xaynai.rerank(vec![], vec![js_document]).unwrap();
         assert_eq!(ranks, [0]);
 
-        let ser = xaynai_serialize(&xaynai).unwrap();
+        let ser = xaynai.serialize().unwrap();
         assert!(ser.length() != 0)
     }
 }
--- xayn-ai-ffi-wasm/src/utils.rs
+++ xayn-ai-ffi-wasm/src/utils.rs
@@ -1,14 +1,27 @@
+use serde::Serialize;
 use wasm_bindgen::JsValue;
+
+#[derive(Serialize)]
+pub struct ExternError {
+    pub code: i32,
+    pub msg: String,
+}
 
 pub trait IntoJsResult<T> {
     fn into_js_result(self) -> Result<T, JsValue>;
 }
 
-impl<T, E> ToJsResult<T> for Result<T, E>
+impl<T, E> IntoJsResult<T> for Result<T, E>
 where
-    E: std::fmt::Debug,
+    E: std::fmt::Display,
 {
-    fn to_js_result(self) -> Result<T, JsValue> {
-        self.map_err(|e| JsValue::from_str(&format!("{:#?}", e)))
+    fn into_js_result(self) -> Result<T, JsValue> {
+        self.map_err(|e| JsValue::from_str(&format!("{}", e)))
+    }
+}
+
+impl<T> IntoJsResult<T> for Result<T, ExternError> {
+    fn into_js_result(self) -> Result<T, JsValue> {
+        self.map_err(|e| JsValue::from_serde(&e).unwrap())
     }
 }
