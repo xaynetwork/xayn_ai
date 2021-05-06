f3c4882695c2374d1e309bb0d995ad09406cd9fc
Philipp Korber <philipp@korber.dev>
2021-05-06 18:10:05 +0200

    Initial incomplete implementation.
    * impl. softmax based on searchgarden ndlayers softmax
    * adapted tests from searchgarden ndlayers softmax
    * initial Dense2D, Dense1D implementations (for 2D,1D
      inputs).
    * some utilities
    * initial list net impl.
    * tests for some utilities
    * some other parts where not comitted as they are
      incomplete.

---
+++ list-net/src/ndutils.rs
@@ -0,0 +1,146 @@
+//! Inspired by  Xayn network : Searchgarden/test_bert/src/ndlayers/
+
+pub mod nn_layer;
+mod softmax;
+
+use ndarray::{Array, ArrayBase, Axis, Data, LinalgScalar, NdFloat, RemoveAxis};
+pub use softmax::*;
+
+/// A python like index where negative values can be used to index from the end.
+///
+/// Do not use this for performance sensitive code, like iteratively indexing an array.
+///
+/// # Panic
+///
+/// It's asserted that all of following hold:
+///
+/// - idx < len (out of bounds)
+/// - -idx <= len (index from back out of bounds)
+/// - len <= isize::MAX (necessary for overflow check -isize::MIN > isize::MAX)
+/// - idx > isize::MIN  (necessary for overflow check)
+///
+/// if that is not true this will panic.
+#[inline]
+pub fn pylike_idx(idx: isize, len: usize) -> usize {
+    assert!(len <= isize::MAX as usize && idx > isize::MIN);
+    let len = len as isize;
+    assert!(idx < len && -idx <= len);
+
+    if idx < 0 {
+        (len + idx) as usize
+    } else {
+        idx as usize
+    }
+}
+
+/// Create a sum over an axis returning a an array with the same number of dimensions.
+///
+/// The dimension of the axis is set to 1.
+///
+/// This means it can be used for broadcasting add/sub/mul/etc.
+#[inline]
+pub fn sum_axis_same_rank<A, S, D>(array: &ArrayBase<S, D>, axis: Axis) -> Array<A, D>
+where
+    A: LinalgScalar,
+    S: Data<Elem = A>,
+    D: RemoveAxis,
+{
+    let mut shape = array.raw_dim();
+    shape[axis.index()] = 1;
+
+    array.sum_axis(axis).into_shape(shape).unwrap()
+}
+
+/// Finds the maximum over an axis returning an array with the same number of dimensions.
+///
+/// The dimension of the axis is set to 1.
+///
+/// This means it can be used for broadcasting add/sub/mul/etc.
+#[inline]
+pub fn max_axis_same_rank<A, S, D>(array: &ArrayBase<S, D>, axis: Axis) -> Array<A, D>
+where
+    A: NdFloat,
+    S: Data<Elem = A>,
+    D: RemoveAxis,
+{
+    let mut shape = array.raw_dim();
+    shape[axis.index()] = 1;
+    array
+        .fold_axis(axis, A::min_value(), |state, val| A::max(*state, *val))
+        .into_shape(shape)
+        .unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use std::panic::catch_unwind;
+
+    use ndarray::arr3;
+
+    use super::*;
+
+    #[test]
+    fn test_pylike_idx_panics_on_overflow() {
+        catch_unwind(|| pylike_idx(0, usize::MAX)).unwrap_err();
+        catch_unwind(|| pylike_idx(isize::MAX, 1)).unwrap_err();
+    }
+
+    #[test]
+    fn test_pylike_idx_panics_on_out_of_bounds() {
+        catch_unwind(|| pylike_idx(3, 3)).unwrap_err();
+        catch_unwind(|| pylike_idx(10, 3)).unwrap_err();
+        catch_unwind(|| pylike_idx(-4, 3)).unwrap_err();
+    }
+
+    #[test]
+    fn test_pylike_idx_returns_the_right_idx() {
+        assert_eq!(pylike_idx(1, 10), 1);
+        assert_eq!(pylike_idx(0, 10), 0);
+        assert_eq!(pylike_idx(9, 10), 9);
+        assert_eq!(pylike_idx(-10, 10), 0);
+        assert_eq!(pylike_idx(-1, 10), 9);
+        assert_eq!(pylike_idx(-9, 10), 1);
+    }
+
+    #[test]
+    fn test_max_axis_same_rank_can_be_used_over_all_axis() {
+        let array = arr3(&[
+            [[1.0f32, 0.], [2., 4.]],
+            [[4., 7.], [3., 3.]],
+            [[0., 2.], [6., 2.]],
+        ]);
+
+        let res = max_axis_same_rank(&array, Axis(0));
+        let expected = arr3(&[[[4.0f32, 7.], [6., 4.]]]);
+        assert_ndarray_eq!(f32, res, expected, ulps = 0);
+
+        let res = max_axis_same_rank(&array, Axis(1));
+        let expected = arr3(&[[[2.0f32, 4.0]], [[4.0, 7.0]], [[6.0, 2.0]]]);
+        assert_ndarray_eq!(f32, res, expected, ulps = 0);
+
+        let res = max_axis_same_rank(&array, Axis(2));
+        let expected = arr3(&[[[1.0f32], [4.]], [[7.], [3.]], [[2.], [6.]]]);
+        assert_ndarray_eq!(f32, res, expected, ulps = 0);
+    }
+
+    #[test]
+    fn test_sum_axis_same_rank_can_be_used_over_all_axis() {
+        let array = arr3(&[
+            [[1.0f32, 0.], [2., 4.]],
+            [[4., 7.], [3., 3.]],
+            [[0., 2.], [6., 2.]],
+        ]);
+
+        let res = sum_axis_same_rank(&array, Axis(0));
+        let expected = arr3(&[[[5.0f32, 9.], [11., 9.]]]);
+        assert_ndarray_eq!(f32, res, expected, ulps = 0);
+
+        let res = sum_axis_same_rank(&array, Axis(1));
+        let expected = arr3(&[[[3.0f32, 4.0]], [[7.0, 10.0]], [[6.0, 4.0]]]);
+        assert_ndarray_eq!(f32, res, expected, ulps = 0);
+
+        let res = sum_axis_same_rank(&array, Axis(2));
+        let expected = arr3(&[[[1.0f32], [6.]], [[11.], [6.]], [[2.], [8.]]]);
+        assert_ndarray_eq!(f32, res, expected, ulps = 0);
+    }
+}
---
+++ list-net/src/ndutils/softmax.rs
@@ -0,0 +1,215 @@
+use ndarray::{ArrayBase, Axis, DataMut, DataOwned, Dimension, NdFloat, RemoveAxis};
+
+use super::{max_axis_same_rank, pylike_idx, sum_axis_same_rank};
+
+/// Computes softmax along specified axis (can use python like negative indices).
+///
+/// Inspired by [autograd's softmax implementation], especially the trick to subtract the max value
+/// to reduce the chance of an overflow.
+///
+/// [autograd softmax]: https://docs.rs/autograd/1.0.0/src/autograd/ops/activation_ops.rs.html#59
+pub fn softmax<A, S, D>(array: ArrayBase<S, D>, axis: isize) -> ArrayBase<S, D>
+where
+    A: NdFloat,
+    S: DataOwned<Elem = A> + DataMut,
+    D: Dimension + RemoveAxis,
+{
+    let axis = Axis(pylike_idx(axis, array.ndim()));
+    // Subtract `max` to prevent overflow, this seems to
+    // not affect the outcome of the softmax.
+    let max = max_axis_same_rank(&array, axis);
+    let mut tmp = array - max;
+
+    // Standard 3step softmax, 1) exp(x), 2) sum up, 3) divide through sum
+    tmp.mapv_inplace(|v| v.exp());
+    let sum = sum_axis_same_rank(&tmp, axis);
+    tmp / sum
+}
+
+#[cfg(test)]
+mod tests {
+    use ndarray::{arr1, arr2, arr3};
+
+    use super::*;
+
+    #[test]
+    fn test_softmax_1d() {
+        let arr = arr1(&[-1_f32, 0., 1., 2., 3.]);
+
+        // axis 0
+        let res = arr1(&[
+            0.011656231_f32,
+            0.03168492,
+            0.08612854,
+            0.23412167,
+            0.6364086,
+        ]);
+        assert_ndarray_eq!(f32, softmax(arr, 0), res);
+    }
+
+    #[test]
+    fn test_softmax_2d() {
+        let arr = arr2(&[
+            [-1_f32, 0., 1., 2., 3.],
+            [9., 8., 7., 6., 5.],
+            [1., -1., 1., -1., 1.],
+        ])
+        .into_shared();
+
+        // axis 0
+        let res = arr2(&[
+            [
+                0.000045382647_f32,
+                0.00033530878,
+                0.0024665247,
+                0.017970119,
+                0.11731042,
+            ],
+            [0.99961925, 0.9995414, 0.995067, 0.9811352, 0.8668133],
+            [
+                0.00033533492,
+                0.00012335321,
+                0.0024665247,
+                0.0008946795,
+                0.01587624,
+            ],
+        ]);
+        assert_ndarray_eq!(f32, softmax(arr.clone(), 0), res);
+
+        // axis 1
+        let res = arr2(&[
+            [
+                0.011656231_f32,
+                0.03168492,
+                0.08612854,
+                0.23412167,
+                0.6364086,
+            ],
+            [0.6364086, 0.23412167, 0.08612854, 0.03168492, 0.011656231],
+            [0.3057477, 0.04137845, 0.3057477, 0.04137845, 0.3057477],
+        ]);
+        assert_ndarray_eq!(f32, softmax(arr, 1), res);
+    }
+
+    #[test]
+    fn test_softmax_3d() {
+        let arr = arr3(&[
+            [
+                [-1_f32, 0., 1., 2., 3.],
+                [9., 8., 7., 6., 5.],
+                [1., -1., 1., -1., 1.],
+            ],
+            [
+                [1., 1., 1., 1., 1.],
+                [2., 2., 2., 2., 2.],
+                [3., 3., 3., 3., 3.],
+            ],
+        ])
+        .into_shared();
+
+        // axis 0
+        let res = arr3(&[
+            [
+                [0.11920292_f32, 0.26894143, 0.5, 0.7310586, 0.880797],
+                [0.999089, 0.9975274, 0.9933072, 0.98201376, 0.95257413],
+                [0.11920292, 0.01798621, 0.11920292, 0.01798621, 0.11920292],
+            ],
+            [
+                [0.880797, 0.7310586, 0.5, 0.26894143, 0.11920292],
+                [
+                    0.00091105123,
+                    0.0024726233,
+                    0.006692851,
+                    0.01798621,
+                    0.047425874,
+                ],
+                [0.880797, 0.98201376, 0.880797, 0.98201376, 0.880797],
+            ],
+        ]);
+        assert_ndarray_eq!(f32, softmax(arr.clone(), 0), res);
+
+        // axis 1
+        let res = arr3(&[
+            [
+                [
+                    0.000045382647_f32,
+                    0.00033530878,
+                    0.0024665247,
+                    0.017970119,
+                    0.11731042,
+                ],
+                [0.99961925, 0.9995414, 0.995067, 0.9811352, 0.8668133],
+                [
+                    0.00033533492,
+                    0.00012335321,
+                    0.0024665247,
+                    0.0008946795,
+                    0.01587624,
+                ],
+            ],
+            [
+                [0.09003057, 0.09003057, 0.09003057, 0.09003057, 0.09003057],
+                [0.24472848, 0.24472848, 0.24472848, 0.24472848, 0.24472848],
+                [0.66524094, 0.66524094, 0.66524094, 0.66524094, 0.66524094],
+            ],
+        ]);
+        assert_ndarray_eq!(f32, softmax(arr.clone(), 1), res);
+
+        // axis 2
+        let res = arr3(&[
+            [
+                [
+                    0.011656232_f32,
+                    0.03168492,
+                    0.08612854,
+                    0.23412167,
+                    0.6364086,
+                ],
+                [0.6364086, 0.23412167, 0.08612854, 0.03168492, 0.011656231],
+                [0.3057477, 0.04137845, 0.3057477, 0.04137845, 0.3057477],
+            ],
+            [
+                [0.2, 0.2, 0.2, 0.2, 0.2],
+                [0.2, 0.2, 0.2, 0.2, 0.2],
+                [0.2, 0.2, 0.2, 0.2, 0.2],
+            ],
+        ]);
+        assert_ndarray_eq!(f32, softmax(arr, 2), res);
+    }
+
+    #[test]
+    fn test_softmax_edgecases() {
+        // 2D axis 0
+        let arr = arr2(&[[-1_f32, 0., 1., 2., 3.]]);
+        let res = arr2(&[[1_f32, 1., 1., 1., 1.]]);
+        assert_ndarray_eq!(f32, softmax(arr, 0), res);
+
+        // 2D axis 1
+        let arr = arr2(&[[-1_f32], [9.], [1.]]);
+        let res = arr2(&[[1_f32], [1.], [1.]]);
+        assert_ndarray_eq!(f32, softmax(arr, 1), res);
+
+        // 3D axis 0
+        let arr = arr3(&[[
+            [-1_f32, 0., 1., 2., 3.],
+            [9., 8., 7., 6., 5.],
+            [1., -1., 1., -1., 1.],
+        ]]);
+        let res = arr3(&[[
+            [1_f32, 1., 1., 1., 1.],
+            [1., 1., 1., 1., 1.],
+            [1., 1., 1., 1., 1.],
+        ]]);
+        assert_ndarray_eq!(f32, softmax(arr, 0), res);
+
+        // 3D axis 1
+        let arr = arr3(&[[[-1_f32, 0., 1., 2., 3.]], [[1., 1., 1., 1., 1.]]]);
+        let res = arr3(&[[[1_f32, 1., 1., 1., 1.]], [[1., 1., 1., 1., 1.]]]);
+        assert_ndarray_eq!(f32, softmax(arr, 1), res);
+
+        // 3D axis 2
+        let arr = arr3(&[[[-1_f32], [9.], [1.]], [[1.], [2.], [3.]]]);
+        let res = arr3(&[[[1_f32], [1.], [1.]], [[1.], [1.], [1.]]]);
+        assert_ndarray_eq!(f32, softmax(arr, 2), res);
+    }
+}
