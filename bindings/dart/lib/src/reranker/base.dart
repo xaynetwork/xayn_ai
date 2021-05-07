9432c6e65f9bc4112cb5b66e833b21333072a1ba
Jan Petsche <58227040+janpetschexain@users.noreply.github.com>
2021-05-07 15:28:37 +0200

    remove ffi dep from base

--- bindings/dart/lib/src/reranker/base.dart
+++ bindings/dart/lib/src/reranker/base.dart
@@ -1,12 +1,12 @@
 import 'dart:typed_data' show Uint8List;
 
-import 'package:xayn_ai_ffi_dart/src/data/document.dart' show Document;
-import 'package:xayn_ai_ffi_dart/src/data/history.dart' show History;
-
 abstract class XaynAi {
   factory XaynAi(dynamic vocab, dynamic model, [dynamic? serialized]) =>
       throw UnsupportedError('Plattform not supported');
-  List<int> rerank(List<History> histories, List<Document> documents) =>
+  List<int> rerank(
+    covariant List<dynamic> histories,
+    covariant List<dynamic> documents,
+  ) =>
       throw UnsupportedError('Plattform not supported');
   Uint8List serialize() => throw UnsupportedError('Plattform not supported');
   List<String> faults() => throw UnsupportedError('Plattform not supported');
