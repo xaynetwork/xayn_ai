8a0ceec7640a5f19aa616b9bbdafbeeca24cbbe1
Philipp Korber <philipp@korber.dev>
2021-05-12 01:00:44 +0200

    Added shape checked in list-net during loading.

--- list-net/src/ndlayers/dense.rs
+++ list-net/src/ndlayers/dense.rs
@@ -76,6 +76,39 @@ where
         }
     }
 
+    pub(crate) fn check_in_out_shapes<D>(&self, mut shape: D) -> Result<D, IncompatibleMatrices>
+    where
+        D: Dimension,
+    {
+        let ndim = shape.ndim();
+        let name_left = "dense/input";
+        let name_right = "dense/weights";
+        let shape_right = self.weights.raw_dim();
+        match ndim {
+            1 | 2 => {
+                if shape[ndim - 1] != shape_right[0] {
+                    Err(IncompatibleMatrices {
+                        name_left,
+                        shape_left: shape.into_dyn(),
+                        name_right,
+                        shape_right: shape_right.into_dyn(),
+                        hint: "input matrix can't be dot multipled with weight matrix",
+                    })
+                } else {
+                    shape[ndim - 1] = shape_right[1];
+                    Ok(shape)
+                }
+            }
+            _ => Err(IncompatibleMatrices {
+                name_left,
+                shape_left: shape.into_dyn(),
+                name_right,
+                shape_right: shape_right.into_dyn(),
+                hint: "can only use dot product with 1 or 2 dimensions arrays",
+            }),
+        }
+    }
+
     pub(crate) fn run<S, D>(&self, input: ArrayBase<S, D>) -> Array<f32, D>
     where
         S: Data<Elem = f32>,
@@ -89,7 +122,7 @@ where
 
 #[cfg(test)]
 mod tests {
-    use ndarray::{arr1, arr2, Array1, Array2};
+    use ndarray::{arr1, arr2, Array1, Array2, IntoDimension};
 
     use crate::ndlayers::activation::{Linear, Relu};
 
@@ -161,5 +194,22 @@ mod tests {
         let weights = Array2::<f32>::ones((2, 3));
         let bias = Array1::ones((2,));
         assert!(Dense::new(weights, bias, Linear::default()).is_err());
+    }
+
+    #[test]
+    fn test_check_in_out_shape() {
+        let weights = Array2::<f32>::ones((5, 4));
+        let bias = Array1::<f32>::ones((4,));
+        let dense = Dense::new(weights, bias, Linear::default()).unwrap();
+
+        let dim = [10, 5].into_dimension();
+        let out_shape = dense.check_in_out_shapes(dim).unwrap();
+        assert_eq!(out_shape, [10, 4].into_dimension());
+
+        let dim = [1, 10, 5].into_dimension();
+        dense.check_in_out_shapes(dim).unwrap_err();
+
+        let dim = [10, 4].into_dimension();
+        dense.check_in_out_shapes(dim).unwrap_err();
     }
 }
