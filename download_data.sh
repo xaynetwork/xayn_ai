b3b970de6a57aa196280514596af8d127e77f5f2
Philipp Korber <philipp@korber.dev>
2021-05-20 00:19:02 +0200

    Added more quotes in download_data.sh.

--- download_data.sh
+++ download_data.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # path to this file
-SELF_PATH=$(readlink -f "$0")
+SELF_PATH="$(readlink -f "$0")"
 # path to the directory where this file is
-SELF_DIR_PATH=$(dirname "$SELF_PATH")
+SELF_DIR_PATH="$(dirname "$SELF_PATH")"
 
 # in this way we can call the script from different directory
 # but the data should go in the correct destination
@@ -13,20 +13,20 @@ CHECKSUM_FILE="sha256sums"
 
 download()
 {
-  NAME=$1
-  VERSION=$2
+  NAME="$1"
+  VERSION="$2"
   ARCHIVE_BASENAME="${NAME}_$VERSION"
   ARCHIVE_NAME="$ARCHIVE_BASENAME.tgz"
   URL="http://s3-de-central.profitbricks.com/xayn-yellow-bert/$NAME/$ARCHIVE_NAME"
 
-  curl $URL -o $DATA_DIR/$ARCHIVE_NAME
+  curl "$URL" -o "$DATA_DIR/$ARCHIVE_NAME"
 
-  cd $DATA_DIR
-  tar -zxf $ARCHIVE_NAME
+  cd "$DATA_DIR"
+  tar -zxf "$ARCHIVE_NAME"
 
   # check content
-  cd $ARCHIVE_BASENAME
-  shasum -c $CHECKSUM_FILE
+  cd "$ARCHIVE_BASENAME"
+  shasum -c "$CHECKSUM_FILE"
 }
 
 download rubert v0001
