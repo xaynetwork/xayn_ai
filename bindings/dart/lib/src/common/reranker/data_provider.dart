42476fdd2442e6e3729d1b893508a5789a987f37
Robert Steiner <robertt.debug@gmail.com>
2021-06-14 17:15:10 +0200

    TY-1830 auto-generate asset artifacts (#97)
    * auto-generate assets metadata
    
    * increase timeout of flutter build

--- bindings/dart/lib/src/common/reranker/data_provider.dart
+++ bindings/dart/lib/src/common/reranker/data_provider.dart
@@ -1,28 +1,7 @@
 import 'dart:convert' show base64;
 import 'package:hex/hex.dart' show HEX;
 
-/// Base assets that are required for both mobile and web.
-///
-/// The checksum is the sha256 hash of the asset.
-/// To calculate the checksum run 'shasum -a 256 vocab.txt'.
-final baseAssets = <AssetType, Asset>{
-  AssetType.smbertVocab: Asset('smbert_v0000/vocab.txt',
-      'ffb2398810fa977ee39e8a0103b07f9c2614f1f9825df53a2c99e71569e3451c'),
-  AssetType.smbertModel: Asset('smbert_v0000/smbert.onnx',
-      'c07653a3cc568876071de157f0fb8ccc1a302197ddad9ead069a86140e19d913'),
-  AssetType.qambertVocab: Asset('qambert_v0001/vocab.txt',
-      'ffb2398810fa977ee39e8a0103b07f9c2614f1f9825df53a2c99e71569e3451c'),
-  AssetType.qambertModel: Asset('qambert_v0001/qambert.onnx',
-      '030e7d68cc82f59640c6b989f145d6eaa7609c8615bd6ae9e7890e1a68be5b71'),
-};
-
-enum AssetType {
-  smbertVocab,
-  smbertModel,
-  qambertVocab,
-  qambertModel,
-  wasmModule
-}
+part 'assets.dart';
 
 class Asset {
   final String suffix;
