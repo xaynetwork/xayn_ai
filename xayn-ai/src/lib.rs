ccdaf2fec874f6d21ca12e025e0717b2e02989df
Andrea Corradi <andrea.corradi@xayn.com>
2021-06-30 15:21:19 +0200

    Update xayn-ai/src/lib.rs
    Co-authored-by: janpetschexain <58227040+janpetschexain@users.noreply.github.com>

--- xayn-ai/src/lib.rs
+++ xayn-ai/src/lib.rs
@@ -38,9 +38,9 @@ pub use crate::{
 #[cfg(test)]
 mod tests;
 
-// we need to export rstest_reuse from the root for it to works.
-// `use rstest_reuse` will trigger clippy::single_component_path_imports
-// that is not possible to silence.
+// we need to export rstest_reuse from the root for it to work.
+// `use rstest_reuse` will trigger `clippy::single_component_path_imports`
+// which is not possible to silence.
 #[cfg(test)]
 #[allow(unused_imports)]
 pub(crate) use rstest_reuse as rstest_reuse;
