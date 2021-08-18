7ea375f7b91b32446081cb61b2d3da8a36627059
Robert Steiner <robertt.debug@gmail.com>
2021-08-18 12:02:12 +0200

    add rayon for running bert models in parallel (#177)

--- xayn-ai/src/embedding/smbert.rs
+++ xayn-ai/src/embedding/smbert.rs
@@ -6,14 +6,20 @@ use crate::{
     reranker::systems::SMBertSystem,
 };
 
+#[cfg(feature = "parallel")]
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
+
 impl SMBertSystem for SMBert {
     fn compute_embedding(
         &self,
         documents: Vec<DocumentDataWithDocument>,
     ) -> Result<Vec<DocumentDataWithSMBert>, Error> {
-        // TODO: optional parallelization
+        #[cfg(not(feature = "parallel"))]
+        let documents = documents.into_iter();
+        #[cfg(feature = "parallel")]
+        let documents = documents.into_par_iter();
+
         documents
-            .into_iter()
             .map(|document| {
                 let embedding = self.run(document.document_content.title.as_str());
                 embedding
