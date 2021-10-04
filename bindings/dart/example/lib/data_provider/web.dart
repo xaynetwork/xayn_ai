ca41a27e6536ace6b20c4ab441a6eb758ca627e2
Robert Steiner <robertt.debug@gmail.com>
2021-10-04 10:11:08 +0200

    TY-2107 load genesis.js in dart (#226)
    * load genesis file in dart code
    
    * make base url relative

--- bindings/dart/example/lib/data_provider/web.dart
+++ bindings/dart/example/lib/data_provider/web.dart
@@ -1,5 +1,7 @@
-import 'dart:html' show window;
-import 'dart:typed_data' show Uint8List, ByteBuffer;
+import 'dart:async' show Completer;
+import 'dart:html' show document, window;
+import 'dart:js' show context;
+import 'dart:typed_data' show ByteBuffer, Uint8List;
 
 import 'package:xayn_ai_ffi_dart/package.dart'
     show AssetType, getAssets, SetupData, WebFeature;
@@ -7,7 +9,7 @@ import 'package:xayn_ai_ffi_dart/package.dart'
 import 'package:xayn_ai_ffi_dart_example/data_provider/data_provider.dart'
     show joinPaths;
 
-const _baseAssetUrl = 'assets/assets';
+const _baseAssetUrl = './assets/assets';
 
 /// Prepares and returns the data that is needed to init [`XaynAi`].
 Future<SetupData> getInputData() async {
@@ -23,8 +25,15 @@ Future<SetupData> getInputData() async {
 
   for (var asset in getAssets(features: features).entries) {
     final path = joinPaths([_baseAssetUrl, asset.value.urlSuffix]);
+    // We also load the wasm script here in order to check its integrity/checksum.
+    // The browser keeps it in cache so `injectWasmScript` does not download it again.
     final data = await _fetchAsset(path, asset.value.checksum.checksumSri);
-    fetched.putIfAbsent(asset.key, () => data);
+
+    if (asset.key == AssetType.wasmScript) {
+      await injectWasmScript(path);
+    } else {
+      fetched.putIfAbsent(asset.key, () => data);
+    }
   }
 
   return SetupData(fetched);
@@ -39,4 +48,22 @@ Future<Uint8List> _fetchAsset(String url, String checksum) async {
   } catch (e) {
     return Future.error('error loading asset: $url, error: $e');
   }
+}
+
+/// Injects the WASM script into the HTML and exports its methods
+/// to the `window` object of the browser.
+Future<void> injectWasmScript(String url) {
+  final completer = Completer<void>();
+  context['_notifyWasmBindingsLoaded'] = () => completer.complete();
+
+  document.head!.append(document.createElement('script')
+    ..attributes = const {'type': 'module'}
+    // ignore: unsafe_html
+    ..setInnerHtml('''
+        import * as xayn_ai_ffi_wasm from '$url';
+        window.xayn_ai_ffi_wasm = xayn_ai_ffi_wasm;
+        _notifyWasmBindingsLoaded();
+     '''));
+
+  return completer.future;
 }
