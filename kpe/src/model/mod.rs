56c1d8db02b933cdcf8da41a4fab7ec056fa2b03
janpetschexain <58227040+janpetschexain@users.noreply.github.com>
2021-10-15 11:20:39 +0200

    TY-2085 kpe prototype [1] (#236)
    * add models

---
+++ kpe/src/model/mod.rs
@@ -0,0 +1,21 @@
+pub mod bert;
+pub mod classifier;
+pub mod cnn;
+
+use std::io::Error as IoError;
+
+use displaydoc::Display;
+use ndarray::ShapeError;
+use thiserror::Error;
+use tract_onnx::prelude::TractError;
+
+/// The potential errors of the models.
+#[derive(Debug, Display, Error)]
+pub enum ModelError {
+    /// Failed to read the onnx model: {0}
+    Read(#[from] IoError),
+    /// Failed to run a tract operation: {0}
+    Tract(#[from] TractError),
+    /// Invalid array shapes: {0}
+    Shape(#[from] ShapeError),
+}
