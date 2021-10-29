0acc0afe8b1de1062d4617667688e632c911a220
Jan Petsche <58227040+janpetschexain@users.noreply.github.com>
2021-10-29 15:57:23 +0200

    rustify

--- layer/src/conv.rs
+++ layer/src/conv.rs
@@ -1,15 +1,25 @@
-use ndarray::{s, Array1, Array3, ArrayBase, Data, Ix3};
+// TODO: refactor looped assignments as iterator collections
+
+use ndarray::{s, Array1, Array2, Array3, ArrayBase, ArrayView2, Data, Ix3};
 
 use crate::utils::IncompatibleMatrices;
 
 pub struct Conv1D {
-    weights: Array3<f32>,
-    bias: Option<Array1<f32>>,
+    // params
+    weights: Array2<f32>,
+    bias: Option<Array3<f32>>,
 
+    // configs
     stride: usize,
     padding: usize,
     dilation: usize,
     groups: usize,
+
+    // shapes
+    channel_out_size: usize,
+    channel_grouped_size: usize,
+    kernel_size: usize,
+    dilated_kernel_size: usize,
 }
 
 impl Conv1D {
@@ -26,159 +36,158 @@ impl Conv1D {
         assert!(stride > 0, "non-positive stride is not supported");
         assert!(dilation > 0, "non-positive dilation is not supported");
         assert!(groups > 0, "non-positive groups is not supported");
+
         let weights_shape = weights.shape();
+        let channel_out_size = weights_shape[0];
+        let channel_grouped_size = weights_shape[1];
+        let kernel_size = weights_shape[2];
+        let dilated_kernel_size = dilation * (kernel_size - 1) + 1;
+
         assert!(
-            groups <= weights_shape[0],
+            groups <= channel_out_size,
             "Given groups={}, expected weight to be at least {} at dimension 0, but got weight of size {:?} instead",
             groups,
             groups,
             weights_shape,
         );
         assert_eq!(
-            weights_shape[0] % groups,
+            channel_out_size % groups,
             0,
             "Given groups={}, expected weight to be divisible by {} at dimension 0, but got weight of size {:?} instead",
             groups,
             groups,
             weights_shape,
         );
-        if let Some(ref bias) = bias {
+        let bias = bias.map(|bias| {
             assert!(!bias.is_empty());
-            let bias_shape = bias.shape();
+            let bias_size = bias.len();
             assert_eq!(
-                bias_shape[0],
-                weights_shape[0],
+                bias_size,
+                channel_out_size,
                 "Given weight of size {:?}, expected bias to be 1-dimensional with {} elements, but got bias of size {:?} instead",
                 weights_shape,
-                weights_shape[0],
-                bias_shape,
+                channel_out_size,
+                bias_size,
             );
-        }
+            bias.into_shape((1, bias_size, 1)).unwrap()
+        });
+        let weights = weights
+            .into_shape((channel_out_size, channel_grouped_size * kernel_size))
+            .unwrap();
 
         Ok(Self {
             weights,
             bias,
+
             stride,
             padding,
             dilation,
             groups,
+
+            channel_out_size,
+            channel_grouped_size,
+            kernel_size,
+            dilated_kernel_size,
         })
     }
 
     // https://github.com/pytorch/pytorch/blob/master/aten/src/ATen/native/Convolution.cpp#L820
+    // https://github.com/pytorch/pytorch/blob/master/aten/src/ATen/native/Convolution.cpp#L1070
     pub fn run<S>(&self, input: ArrayBase<S, Ix3>) -> Array3<f32>
     where
         S: Data<Elem = f32>,
     {
         let input_shape = input.shape();
-        let weights_shape = self.weights.shape();
+        let batch_size = input_shape[0];
+        let channel_in_size = input_shape[1];
+        let input_size = input_shape[2];
+
         assert_eq!(
-            input_shape[1],
-            weights_shape[1] * self.groups,
+            channel_in_size,
+            self.channel_grouped_size * self.groups,
             "Given groups={}, weight of size {:?}, expected input {:?} to have {} channels, but got {} channels instead",
             self.groups,
-            weights_shape,
+            [self.channel_out_size, self.channel_grouped_size, self.kernel_size],
             input_shape,
-            weights_shape[1] * self.groups,
-            input_shape[1],
+            self.channel_grouped_size * self.groups,
+            channel_in_size,
         );
-        let input_size = input_shape[2] + 2 * self.padding;
-        let kernel_size = self.dilation * (weights_shape[2] - 1) + 1;
+        let padded_input_size = input_size + 2 * self.padding;
         assert!(
-            input_size >= kernel_size,
+            padded_input_size >= self.dilated_kernel_size,
             "Calculated padded input size per channel: {}. Kernel size: {}. Kernel size can't be greater than actual input size.",
-            input_size,
-            kernel_size,
+            padded_input_size,
+            self.dilated_kernel_size,
         );
 
-        let output_size = (input_size - kernel_size) / self.stride + 1;
+        let output_size = (padded_input_size - self.dilated_kernel_size) / self.stride + 1;
         if input.is_empty() {
-            return Array3::zeros([0, self.weights.shape()[0], output_size]);
+            return Array3::zeros([0, self.channel_out_size, output_size]);
         }
 
         if self.groups > 1 {
             unimplemented!("https://github.com/pytorch/pytorch/blob/master/aten/src/ATen/native/Convolution.cpp#L1032-L1041");
         }
-
         if self.dilation > 1 {
             unimplemented!("https://github.com/pytorch/pytorch/blob/master/aten/src/ATen/native/NaiveDilatedConvolution.cpp#L434");
         }
 
-        // https://github.com/pytorch/pytorch/blob/master/aten/src/ATen/native/Convolution.cpp#L1070
-        let _kernel_height = 1;
-        let kernel_width = weights_shape[2];
-        let _pad_height = 0;
-        let pad_width = self.padding;
-        let _stride_height = 1;
-        let stride_width = self.stride;
-
-        let n_input_plane = input_shape[1];
-        let _input_height = 1;
-        let input_width = input_shape[2];
-        let n_output_plane = weights_shape[0];
-        let _output_height = 1;
-        let output_width = output_size;
-        let batch_size = input_shape[0];
-
-        // TODO: reshape only once in `new`
-        let weights = self
-            .weights
-            .view()
-            .into_shape((weights_shape[0], weights_shape[1] * weights_shape[2]))
-            .unwrap();
-
-        let mut finput = if kernel_width == 1 && stride_width == 1 && pad_width == 0 {
-            // output_width == input_shape[2]
-            input
-                .to_owned()
-                .into_shape((input_shape[0], input_shape[1], output_width))
-                .unwrap()
-        } else {
-            Array3::zeros((input_shape[0], input_shape[1] * kernel_width, output_width))
-        };
-
         // https://github.com/pytorch/pytorch/blob/master/aten/src/ATen/native/ConvolutionMM2d.cpp#L140
-        let mut output = Array3::<f32>::zeros([
-            batch_size,
-            n_output_plane, /* ws0 */
-            output_width,   /* is2 - ws2 + 1 */
-        ]);
+        let mut output = Array3::zeros([batch_size, self.channel_out_size, output_size]);
         for i in 0..batch_size {
             let input = input.slice(s![i, .., ..]);
-            let mut finput = finput.slice_mut(s![i, .., ..]);
             let mut output = output.slice_mut(s![i, .., ..]);
-            if kernel_width != 1 || stride_width != 1 || pad_width != 0 {
-                let input = input.as_slice().unwrap();
-                let finput = finput.as_slice_mut().unwrap();
-
-                // https://github.com/pytorch/pytorch/blob/master/aten/src/ATen/native/cpu/Unfold2d.cpp#L160
-                for k in 0..n_input_plane * kernel_width {
-                    let nip = k / kernel_width;
-                    let rest = k % kernel_width;
-                    let kh = rest / kernel_width;
-                    let kw = rest % kernel_width;
-
-                    if pad_width > 0 || stride_width > 1 {
-                        unimplemented!("https://github.com/pytorch/pytorch/blob/master/aten/src/ATen/native/cpu/Unfold2d.cpp#L160");
-                    } else {
-                        let src = nip * input_width + kh * input_width + kw;
-                        let dst = nip * kernel_width * output_width
-                            + kh * kernel_width * output_width
-                            + kw * output_width;
-                        finput[dst..dst + output_width]
-                            .copy_from_slice(&input[src..src + output_width]);
-                    }
-                }
+            if self.kernel_size == 1 && self.stride == 1 && self.padding == 0 {
+                output.assign(&self.weights.dot(&input));
+            } else {
+                let input = self.unfold(input, output_size);
+                output.assign(&self.weights.dot(&input));
             }
-
-            output.assign(&weights.dot(&finput));
         }
 
         if let Some(ref bias) = self.bias {
-            output = output + bias.view().into_shape((1, bias.len(), 1)).unwrap();
+            output + bias
+        } else {
+            output
+        }
+    }
+
+    // https://github.com/pytorch/pytorch/blob/master/aten/src/ATen/native/cpu/Unfold2d.cpp#L160
+    fn unfold(&self, input: ArrayView2<f32>, output_size: usize) -> Array2<f32> {
+        if self.padding > 0 {
+            unimplemented!("https://github.com/pytorch/pytorch/blob/master/aten/src/ATen/native/cpu/Unfold2d.cpp#L191-L243");
         }
 
-        output
+        let channel_in_size = input.shape()[0];
+        let input_size = input.shape()[1];
+        let input = input.as_slice().unwrap();
+
+        let mut unfolded = Array2::zeros((channel_in_size * self.kernel_size, output_size));
+        {
+            let unfolded = unfolded.as_slice_mut().unwrap();
+            for k in 0..channel_in_size * self.kernel_size {
+                let nip = k / self.kernel_size;
+                let rest = k % self.kernel_size;
+                let kh = rest / self.kernel_size;
+                let kw = rest % self.kernel_size;
+
+                let src = nip * input_size + kh * input_size + kw;
+                let dst = nip * self.kernel_size * output_size
+                    + kh * self.kernel_size * output_size
+                    + kw * output_size;
+
+                if self.stride > 1 {
+                    for j in 0..output_size {
+                        unfolded[dst + j] = input[src + j * self.stride];
+                    }
+                } else {
+                    unfolded[dst..dst + output_size]
+                        .copy_from_slice(&input[src..src + output_size]);
+                }
+            }
+        }
+
+        unfolded
     }
 }
 
@@ -189,7 +198,7 @@ mod tests {
     use super::*;
     use test_utils::assert_approx_eq;
 
-    // https://github.com/pytorch/pytorch/blob/master/test/cpp/api/functional.cpp#L13
+    // https://github.com/pytorch/pytorch/blob/master/test/cpp/api/functional.cpp#L13-L26
     #[test]
     fn test_conv1d_wide_kernel() {
         let weights = Array1::range(0., 18., 1.).into_shape((2, 3, 3)).unwrap();
@@ -215,7 +224,6 @@ mod tests {
     }
 
     #[test]
-    #[should_panic(expected = "not implemented: https://github.com/pytorch")]
     fn test_conv1d_stride() {
         let weights = Array1::range(0., 18., 1.).into_shape((2, 3, 3)).unwrap();
         let input = Array1::range(0., 30., 1.).into_shape((2, 3, 5)).unwrap();
@@ -224,12 +232,12 @@ mod tests {
             [[312., 384.], [798., 1032.]],
             [[852., 924.], [2553., 2787.]],
         ]);
-        assert_approx_eq!(f32, output, expected);
+        assert_approx_eq!(f32, dbg!(output), expected);
     }
 
     #[test]
     #[should_panic(expected = "not implemented: https://github.com/pytorch")]
-    fn test_conv1d_pad() {
+    fn test_conv1d_padding() {
         let weights = Array1::range(0., 18., 1.).into_shape((2, 3, 3)).unwrap();
         let input = Array1::range(0., 30., 1.).into_shape((2, 3, 5)).unwrap();
         let output = Conv1D::new(weights, None, 1, 1, 1, 1).unwrap().run(input);
