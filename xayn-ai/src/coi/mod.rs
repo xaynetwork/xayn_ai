292c98ad5af8fb6bfd506ca51e264ccf43c21836
Jan Petsche <58227040+janpetschexain@users.noreply.github.com>
2022-01-17 12:47:02 +0100

    move config error

--- xayn-ai/src/coi/mod.rs
+++ xayn-ai/src/coi/mod.rs
@@ -21,9 +21,9 @@ use serde::{Deserialize, Serialize};
 use thiserror::Error;
 use uuid::Uuid;
 
-use crate::embedding::utils::ArcEmbedding;
 #[cfg(test)]
 use crate::tests::mock_uuid;
+use crate::{embedding::utils::ArcEmbedding, ranker::config::Error as ConfigError};
 
 #[repr(transparent)] // needed for FFI
 #[derive(
@@ -40,7 +40,6 @@ impl CoiId {
 }
 
 #[derive(Debug, Display, Error)]
-#[allow(clippy::enum_variant_names)]
 pub(crate) enum CoiError {
     /// A key phrase is empty
     EmptyKeyPhrase,
@@ -48,15 +47,6 @@ pub(crate) enum CoiError {
     NonFiniteKeyPhrase(ArcEmbedding),
     /// A computed relevance score isn't finite.
     NonFiniteRelevance,
-    /// Invalid coi shift factor, expected value from the unit interval
-    InvalidShiftFactor,
-    /// Invalid coi threshold, expected non-negative value
-    #[cfg(test)]
-    InvalidThreshold,
-    /// Invalid coi neighbors, expected positive value
-    InvalidNeighbors,
-    /// Invalid coi gamma, expected value from the unit interval
-    InvalidGamma,
-    /// Invalid coi penalty, expected non-empty, finite and sorted values
-    InvalidPenalty,
+    /// Invalid configuration: {0}
+    InvalidConfiguration(#[from] ConfigError),
 }
