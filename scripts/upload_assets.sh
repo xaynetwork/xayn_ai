4ed038bfe67c8aad89cb26e05e6e480163ce4f7d
Robert Steiner <robertt.debug@gmail.com>
2022-02-09 10:43:29 +0000

    TY-2395 upload assets script (#414)
    Ticket: 
    
    - [TY-2395]
    
    Summary:
    
    - added script to upload the data assets in assets_manifest.json and generate asset_manifest.json (used then by team yellow/blue)
    - moved assets function into its own file
    - renamed `dart_enum_name` to `id`
    - tested on mobile and web example 
    - tested the upload locally with the `kpe_v0001` assets
    - checked [release ci](https://github.com/xaynetwork/xayn_ai/actions/runs/1813151210)
    
    [TY-2395]: https://xainag.atlassian.net/browse/TY-2395?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ scripts/upload_assets.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Generates a manifest file for the data assets in `assets_manifest.json` and uploads them to the
+# given bucket URL. If an asset of the `data_assets` array contains a `chunk_size` key, the script
+# splits the asset into chunks where each chunk has a maximum size of `chunk_size`. The format of
+# the `chunk_size` value is equivalent to the `SIZE` argument in `split` or `gsplit` on macOS.
+# See `split`/`gsplit` man page for more details. The manifest file is written to the `out`
+# directory.
+#
+# Usage:
+# ./upload_assets <path of assets_manifest.json> <bucket url e.g. s3://xayn-yellow-bert>
+set -e
+
+source $(dirname "$0")/assets_generation_lib.sh
+
+ASSET_METADATA=$(gen_data_assets_metadata_only "$1")
+BUCKET_URL="$2"
+
+for ASSET in $(cat "$ASSET_METADATA" | jq -c '.upload[]'); do
+    ASSET_URL_SUFFIX=$(echo $ASSET | jq -r '.url_suffix')
+    ASSET_PATH=$(echo $ASSET | jq -r '.path')
+    s3cmd sync -v --acl-public --guess-mime-type --no-mime-magic --skip-existing $ASSET_PATH ${BUCKET_URL}/$ASSET_URL_SUFFIX
+done
+
+ASSET_MANIFEST="$(dirname "$ASSET_METADATA")/asset_manifest.json"
+jq 'del(.upload)' "$ASSET_METADATA" > $ASSET_MANIFEST
+
+echo "assets manifest path: $ASSET_MANIFEST"
