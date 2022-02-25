e6ed7f7d85848f09b2fd4c3bc01134dab6fa3278
Jan Petsche <58227040+janpetschexain@users.noreply.github.com>
2022-02-25 17:26:09 +0100

    sort by date if few cois

--- xayn-ai/src/ranker/document.rs
+++ xayn-ai/src/ranker/document.rs
@@ -1,4 +1,5 @@
-// Copy of the document struct of the DE
+use chrono::NaiveDateTime;
+
 use crate::{embedding::utils::Embedding, DocumentId};
 
 pub trait Document {
@@ -8,34 +9,25 @@ pub trait Document {
     /// Gets the SMBert embedding of the document.
     fn smbert_embedding(&self) -> &Embedding;
 
-    /// Get the API score.
-    fn score(&self) -> Option<f32>;
-
-    /// Get the API rank.
-    fn rank(&self) -> usize;
+    /// Gets the publishing date.
+    fn date_published(&self) -> NaiveDateTime;
 }
 
 #[cfg(test)]
 pub(super) struct TestDocument {
     pub(super) id: DocumentId,
     pub(super) smbert_embedding: Embedding,
-    pub(super) score: Option<f32>,
-    pub(super) rank: usize,
+    pub(super) date_published: NaiveDateTime,
 }
 
 #[cfg(test)]
 impl TestDocument {
-    pub(super) fn new(
-        id: u128,
-        embedding: impl Into<Embedding>,
-        score: Option<f32>,
-        rank: usize,
-    ) -> Self {
+    pub(super) fn new(id: u128, embedding: impl Into<Embedding>, date_published: &str) -> Self {
         Self {
             id: DocumentId::from_u128(id),
             smbert_embedding: embedding.into(),
-            score,
-            rank,
+            date_published: NaiveDateTime::parse_from_str(date_published, "%Y-%m-%d %H:%M:%S")
+                .unwrap(),
         }
     }
 }
@@ -50,13 +42,7 @@ impl Document for TestDocument {
         &self.smbert_embedding
     }
 
-    /// Get the API score.
-    fn score(&self) -> Option<f32> {
-        self.score
-    }
-
-    /// Get the API rank.
-    fn rank(&self) -> usize {
-        self.rank
+    fn date_published(&self) -> NaiveDateTime {
+        self.date_published
     }
 }
